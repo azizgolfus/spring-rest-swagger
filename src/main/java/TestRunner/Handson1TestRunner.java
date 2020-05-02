47b2a01884ea0c1804caee3490ac8689ba18b32e
Aziz Rahman <rahman786golfus@gmail.com>
2020-05-02 17:26:53 +0530

    added for learning purpose

--- src/main/java/TestRunner/Handson1TestRunner.java
+++ src/main/java/TestRunner/Handson1TestRunner.java
@@ -3,7 +3,7 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
-
+// this is a demo for git hub
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src\\main\\java\\Features\\CucumberHandson1Feature.feature", //the path of the feature files
